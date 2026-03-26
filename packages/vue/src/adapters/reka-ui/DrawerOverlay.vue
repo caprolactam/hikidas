dffaebe0dbd39c3d7e20486aaabffa63538368d5
caprolactam <caprolactam113@gmail.com>
2026-03-26 10:16:44 +0900

    feat: enhance DrawerOverlay component with improved props handling

--- packages/vue/src/adapters/reka-ui/DrawerOverlay.vue
+++ packages/vue/src/adapters/reka-ui/DrawerOverlay.vue
@@ -1,6 +1,17 @@
+<script lang="ts">
+import type { DialogOverlayProps } from 'reka-ui'
+
+export interface DrawerOverlayProps extends Omit<
+  DialogOverlayProps,
+  'forceMount'
+> {}
+</script>
+
 <script setup lang="ts">
 import { DialogOverlay, useForwardExpose } from 'reka-ui'
 import { useDrawerOverlay } from '../../composables'
+
+defineProps<DrawerOverlayProps>()
 
 defineOptions({
   inheritAttrs: false,
@@ -11,5 +22,9 @@ useDrawerOverlay(currentElement)
 </script>
 
 <template>
-  <DialogOverlay v-bind="$attrs" :ref="forwardRef" force-mount />
+  <DialogOverlay
+    v-bind="{ ...$attrs, ...$props }"
+    :force-mount="true"
+    :ref="forwardRef"
+  />
 </template>
